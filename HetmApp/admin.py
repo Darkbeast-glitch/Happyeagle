0df9db33c944a188437a0e0d41ce963871c380ec
Julius Boakye <bbjulius4@gmail.com>
2024-02-22 14:14:27 +0000

    Fix styling issues in gallery and contact pages

--- HetmApp/admin.py
+++ HetmApp/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-from HetmApp.models import TourPackages,MainPackageView,HomePackages,PackageCategory,Gallery
-
+from HetmApp.models import TourPackages, MainPackageView, HomePackages, PackageCategory, Gallery
 
 
 # Register your models here.
-
-
 
 
 admin.site.register(HomePackages)
@@ -15,9 +12,9 @@ admin.site.register(Gallery)
 # Register the MainPackageView model
 @admin.register(MainPackageView)
 class MainPackageViewAdmin(admin.ModelAdmin):
-    list_display = ('package_name', 'package_place', 'package_days', 'category', 'package_price', 'theDate', 'date_added')
+    list_display = ('package_name', 'package_place', 'package_days',
+                    'category', 'package_price', 'theDate', 'date_added')
     # Add more fields as needed for the list view in the admin interface
-
 
 
 # Register the PackageCategory model
@@ -29,11 +26,12 @@ class PackageCategoryAdmin(admin.ModelAdmin):
 
 class TourPackagesAdmin(admin.ModelAdmin):
     list_display = ('tour_package', 'tour_place', 'tour_price', 'payment_id')
-    
+
     def save_model(self, request, obj, form, change):
         # Automatically set tour_price based on MainPackageView when creating/editing TourPackages
         if obj.tour_package and not obj.tour_price:
             obj.tour_price = obj.tour_package.tour_price
         super().save_model(request, obj, form, change)
+
 
 admin.site.register(TourPackages, TourPackagesAdmin)
