2610ac339f1ec551c9dc340dd5ca31c71f453b38
Julius Boakye <bbjulius4@gmail.com>
2024-02-26 07:24:44 +0000

    Fix CSS class name and add scroll position functionality

--- HetmApp/urls.py
+++ HetmApp/urls.py
@@ -1,22 +1,21 @@
-from django.urls import path,include
-from HetmApp.views import HomepageView, BookingView,PackageView,AboutView,ContactView,SelectCurrency,SuccessPage,GalleryPage
+from django.urls import path
+from HetmApp.views import HomepageView, BookingView, PackageView, AboutView, ContactView, SelectCurrency, SuccessPage, GalleryPage
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', HomepageView, name="home" ),
-    path('booking/', BookingView, name="booking" ),
-    path('packages/', PackageView, name="packages" ),
-    path('aboutus/', AboutView, name="about" ),
-    path('contact/', ContactView, name="contact" ),
+    path('', HomepageView, name="home"),
+    path('booking/', BookingView, name="booking"),
+    path('packages/', PackageView, name="packages"),
+    path('aboutus/', AboutView, name="about"),
+    path('contact/', ContactView, name="contact"),
     path('selectcurrency/', SelectCurrency, name='selectcurrency'),
     path('success/', SuccessPage, name='success'),
     path('gallery/', GalleryPage, name='gallery'),
 
-    
+
 
 ]
 if settings.DEBUG:
-  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
